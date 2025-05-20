01d492902d854ff3079d025c4793555bc5deda6f
Danishan <danishan089@gamil.com>
2025-05-20 22:54:32 +0530

    feat: Add UICards export for card components

--- src/export.js
+++ src/export.js
@@ -1,9 +1,20 @@
 import { Alert } from "./Alert/js/Alert";
 import { AlertContainer } from "./Alert/js/AlertContainer";
+import { CardImg } from "./Cards/js/CardImg";
+import { CardSimpleInfo } from "./Cards/js/CardSimpleInfo";
+import { CardTextOn } from "./Cards/js/CardTextOn";
+import { CardTextSliding } from "./Cards/js/CardTextSliding";
 import { useAlerts } from "./Hooks/useAlert";
 
 export const UIAlert = {
   Alert,
   AlertContainer,
   useAlerts,
+};
+
+export const UICards = {
+  CardImg,
+  CardSimpleInfo,
+  CardTextOn,
+  CardTextSliding,
 };
