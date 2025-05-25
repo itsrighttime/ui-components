13c489a4cc2ff20aa60b85faef3e0b1512e5f735
Danishan <danishan089@gamil.com>
2025-05-25 18:00:04 +0530

    feat: format Tabs component for improved readability; adjust FlexContainer props layout

--- src/Layout/Workspace/helper/Tabs.jsx
+++ src/Layout/Workspace/helper/Tabs.jsx
@@ -4,10 +4,16 @@ import { Tab } from "./Tab";
 
 export const Tabs = ({ tabs, direction }) => {
   return (
-    <FlexContainer direction={direction} justify="center" align="center" className={styles.navigatorSection}>
+    <FlexContainer
+      direction={direction}
+      justify="center"
+      align="center"
+      className={styles.navigatorSection}
+    >
       {tabs.map((tab) => (
         <Tab
           key={tab.key}
+          mykey={tab.key}
           value={tab.value}
           icon={tab.icon}
           onClick={tab.onClick}
