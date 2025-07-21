36b316bbc52bfeef8b275e909252626fd5d8f162
Danishan Farookh <danishan089@gamil.com>
2025-07-21 12:29:13 +0530

    fix: vite config error

---
+++ vite.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve("src/export.js"), // or export.jsx
+      name: "ItsRightTimeUI",
+      formats: ["es", "cjs"],
+      fileName: (format) => `ui-component.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
