9d2c16254ff166207c70e2f940fcd261b67b71d0
Danishan Farookh <danishan089@gamil.com>
2025-12-11 22:54:46 +0530

    update

--- tsup.config.js
+++ tsup.config.js
@@ -1,96 +1,18 @@
 import { defineConfig } from "tsup";
-import postcss from "postcss";
-import postcssModules from "postcss-modules";
-import fs from "fs";
-
-const cssModuleMap = {};
 
 export default defineConfig({
-  entry: ["src/export.js"],
+  entry: ["src/export.js"],   // or index.js (use your real entry)
   format: ["esm", "cjs"],
-  dts: false,
-  splitting: false,
-  sourcemap: false,
+  outDir: "dist",
   clean: true,
   external: ["react", "react-dom"],
+  cssModules: true,
+  minify: true,
+  legacyOutput: true,
 
-  loader: {
-    ".png": "file",
-    ".jpg": "file",
-    ".svg": "file",
-  },
-
-  async onSuccess() {
-    // nothing here
-  },
-
+  // Most important part:
   esbuildOptions(options) {
-    // Enable CSS loader
-    options.loader = {
-      ...options.loader,
-      ".css": "copy",
-    };
-  },
-
-  /**
-   * IMPORTANT:
-   * custom loader for CSS Modules
-   */
-  async banner() {
-    return {
-      js: `
-        import "./export.css";
-      `,
-    };
-  },
-
-  /**
-   * Hook: process CSS Modules manually
-   */
-  async beforeBuild() {
-    const walkSync = function (dir, filelist = []) {
-      const files = fs.readdirSync(dir);
-      files.forEach((file) => {
-        if (fs.statSync(dir + "/" + file).isDirectory()) {
-          filelist = walkSync(dir + "/" + file, filelist);
-        } else {
-          if (file.endsWith(".module.css")) {
-            filelist.push(dir + "/" + file);
-          }
-        }
-      });
-      return filelist;
-    };
-
-    const cssFiles = walkSync("./src");
-
-    let finalCSS = "";
-
-    for (let file of cssFiles) {
-      const rawCSS = fs.readFileSync(file, "utf8");
-
-      await postcss([
-        postcssModules({
-          generateScopedName: "[name]__[local]__[hash:base64:5]",
-
-          getJSON(cssFileName, json) {
-            const key = cssFileName.replace("src/", "");
-            cssModuleMap[key] = json;
-          },
-        }),
-      ])
-        .process(rawCSS, { from: file })
-        .then((result) => {
-          finalCSS += result.css + "\n";
-        });
-    }
-
-    fs.writeFileSync("dist/export.css", finalCSS);
-
-    // write classmap
-    fs.writeFileSync(
-      "dist/module-classmap.json",
-      JSON.stringify(cssModuleMap, null, 2)
-    );
-  },
+    options.jsx = "automatic";
+    options.jsxImportSource = "react";
+  }
 });
